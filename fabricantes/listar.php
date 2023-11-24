9b485e3e91ffa476e8011ffa798a0d306262f240
s0pa-fria <130664417+s0pa-fria@users.noreply.github.com>
2023-11-24 15:09:09 -0300

    fim.estilisação

--- fabricantes/listar.php
+++ fabricantes/listar.php
@@ -8,6 +8,12 @@ $listaDeFabricantes = lerFabricantes($conexao);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="../style.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
     <title>Fabricantes</title>
 </head>
 <body>
@@ -16,7 +22,7 @@ $listaDeFabricantes = lerFabricantes($conexao);
         <hr>
         <h2>Lendo e carregando todos os fabricantes</h2>
 
-        <p><a href="inserir.php" style="color:Blue">Inserir um novo fabricante</a></p>
+        <p><a href="inserir.php" style="color:Blue"><button type="button" class="btn btn-primary">Inserir um novo fabricante</button></a></p>
 
         <?php if(isset($_GET['status'])&& $_GET['status'] == 'sucesso'){?>
         <p>Fabricante atualizado com sucesso!</p>
@@ -43,12 +49,13 @@ $listaDeFabricantes = lerFabricantes($conexao);
                     <td> <?=$fabricante['id']?> </td>
                     <td> <?=$fabricante['nome']?> </td>
 
-                    <td><a href="atualizar.php?id=<?=$fabricante['id']?>" style= "color:blue">Atualizar</a></td>
-                    <td> <a class="escluir" href="excluir.php?id=<?$fabricante['id']?>" style= "color:red">Excluir</a></td>
+                    <td><a href="atualizar.php?id=<?=$fabricante['id']?>" style= "color:blue"><button type="button" class="btn btn-success">Atualizar</button></a></td>
+                    <td> <a class="escluir" href="excluir.php?id=<?$fabricante['id']?>" style= "color:red"><button type="button" class="btn btn-danger">Excluir</button></a></td>
                 </tr>
                 <?php } ?>
             </tbody>
         </table>
+        <p><a href="../index.html"><button type="submit" class="btn btn-warning">Home</button></a></p>
     </div>
 
     <script src="../js/confirm.js"></script>
